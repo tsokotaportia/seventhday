b9300b6f382618317ee0b9273bba9f35dc718c4c
Portia Tsokota <tsokotaportia@gmail.com>
2022-06-06 15:34:15 +0200

    added new features to my pages

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -17,12 +17,12 @@ export class EventsComponent implements OnInit {
 
             },
             {
-              image:"assets/images/city-church-christchurch-ynF4v8W7MIw-unsplash.jpg",
-              cardTitle:"Revival",
+              image:"assets/images/baptism1.jpg",
+              cardTitle:"Baptism",
               description:"The law of the Lord is perfect, refreshing the soul. The statutes of the Lord are trustworthy, making wise the simple."
             },
             {
-              image:"assets/images/city-church-christchurch-ynF4v8W7MIw-unsplash.jpg",
+              image:"assets/images/cambridge.jpg",
               cardTitle:"Evangelism",
               description:"To me, it has been a source of great comfort and strength in the day of battle, just to remember that the secret of steadfastness, and indeed, of victory, is the recognition that the Lord is at hand."
             }
